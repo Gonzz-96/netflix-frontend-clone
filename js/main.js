46e2f81867f11a4b40dafbaf0f8cddc04154574e
Gonzalo Campos <erick.campos@wizeline.com>
2020-06-01 18:24:26 -0500

    Add js logic to show only one tab at a time

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
+
+// Select tab content item
+function selectItem(e) {
+  removeBorder()
+  removeShowClass()
+  // Add border to current tab
+  this.classList.add('tab-border')
+  // grab content item from DOM
+  const tabContentItem = document.querySelector(`#${this.id}-content`)
+  tabContentItem.classList.add('show')
+}
+
+function removeBorder() {
+  tabItems.forEach(item => item.classList.remove('tab-border'))
+}
+
+function removeShowClass() {
+  tabContentItems.forEach(item => item.classList.remove('show'))
+}
+
+// Listeb for tab click
+tabItems.forEach(item => item.addEventListener('click', selectItem))
